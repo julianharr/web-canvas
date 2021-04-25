d2084f3c794410490f6155b6a2e09df91f36a440
Julian Harrington <jrayharrington@gmail.com>
2021-04-25 11:06:43 +1000

    add comments explaining event listeners, shift position of importing script in html

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-const canvas = document.querySelector('#canvas')
+const canvas = document.querySelector('#draw');
 
 const ctx = canvas.getContext('2d');
 
-// Set width and height of drawing area
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// Set style of 'brush'
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
@@ -14,25 +12,31 @@ ctx.lineWidth = 10;
 
 let isDrawing = false;
 let lastX = 0;
-let lastY = 0;
+let lastY = 0
 let hue = 0;
 
+
 function draw(event) {
-  if (!isDrawing) return;
+  if (!isDrawing) return; // stop func from running when mouse is not clicked
+  console.log(event);
   ctx.beginPath();
-  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`
+  // Start from   
   ctx.moveTo(lastX, lastY);
+  // Go to
   ctx.lineTo(event.offsetX, event.offsetY);
   ctx.stroke();
-  [lastX, lastY] = [event.lastX, event.lastY];
+  [lastX, lastY] = [event.offsetX, event.offsetY];
   hue++
 }
 
+// Listen for mousedown event and only draw when mouse is clicked
 canvas.addEventListener('mousedown', (event) => {
   isDrawing = true;
-  [lastX, lastY] = [event.lastX, event.lastY];
+  [lastX, lastY] = [event.offsetX, event.offsetY];
 });
-
 canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
+
+
